19c89da66beb6bb54deb32f8c71249ba1229d5c3
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-22 00:44:51 -0300

    777

--- JavaScript/meuframework.js
+++ JavaScript/meuframework.js
@@ -1,10 +1,18 @@
 function calculaMedia() {
     var total = null;
     for (x = 0; typeof arguments[x] === 'number'; x++) {
-        
+
         total += arguments[x];
     }
 
-    return total / arguments.length;
-    
-} 
+    return (total / arguments.length).toFixed(2);
+
+}
+
+function sortear(n) {
+    var _n = n || 1;
+    var nSorteado = Math.random();
+    nSorteado *= _n;
+    nSorteado = Math.floor(nSorteado);
+    return nSorteado;
+}
